35a53dc0b8af978240072b256a8eb54733b715a0
Tim <timruiterkamp@gmail.com>
2019-03-28 23:01:10 +0100

    heroku fix

--- src/scripts/mapbox.js
+++ src/scripts/mapbox.js
@@ -1,3 +1,37 @@
+const fetch = require("node-fetch");
+
+const url = (end, config = { prefix: "", options: "" }) => {
+  const prefix = config.prefix ? config.prefix : config;
+  const options = config.options ? `?${config.options}` : "/";
+  const key = config.options
+    ? `&access_token=${process.env.MAPBOX_KEY}`
+    : `?access_token=${process.env.MAPBOX_KEY}`;
+  return `https://api.mapbox.com/${end}/${prefix}${options}${key}`;
+};
+const request = url =>
+  new Promise((resolve, reject) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(resolve)
+      .catch(reject);
+  });
+
+module.exports = {
+  searchLocation(query) {
+    const fetchUrl = url("geocoding/v5/mapbox.places", {
+      prefix: `${query}.json`
+    });
+    return request(fetchUrl);
+  },
+  searchRoute(start, end) {
+    const fetchUrl = url("directions/v5/mapbox/walking", {
+      prefix: `${start[0]},${start[1]};${end[0]},${end[1]}`,
+      options: "steps=true"
+    });
+    return request(fetchUrl);
+  }
+};
+
 // if (module) {
 //   export mapbox;
 // } else {
--- src/server.js
+++ src/server.js
@@ -39,8 +39,8 @@ async function directions(req, res, next) {
   }
   const end = [4.909457, 52.359849];
   console.log(mapbox);
-  const location = await searchLocation(req.body.location);
-  const directions = await searchRoute(
+  const location = await mapbox.searchLocation(req.body.location);
+  const directions = await mapbox.searchRoute(
     location ? location.features[0].center : "",
     end
   );
@@ -59,37 +59,4 @@ async function directions(req, res, next) {
     data.push(steps);
   });
   res.render("overview", { directions: data });
-}
-
-const fetch = require("node-fetch");
-
-const url = (end, config = { prefix: "", options: "" }) => {
-  const prefix = config.prefix ? config.prefix : config;
-  const options = config.options ? `?${config.options}` : "/";
-  const key = config.options
-    ? `&access_token=${process.env.MAPBOX_KEY}`
-    : `?access_token=${process.env.MAPBOX_KEY}`;
-  return `https://api.mapbox.com/${end}/${prefix}${options}${key}`;
-};
-const request = url =>
-  new Promise((resolve, reject) => {
-    fetch(url)
-      .then(res => res.json())
-      .then(resolve)
-      .catch(reject);
-  });
-
-function searchLocation(query) {
-  const fetchUrl = url("geocoding/v5/mapbox.places", {
-    prefix: `${query}.json`
-  });
-  return request(fetchUrl);
-}
-
-function searchRoute(start, end) {
-  const fetchUrl = url("directions/v5/mapbox/walking", {
-    prefix: `${start[0]},${start[1]};${end[0]},${end[1]}`,
-    options: "steps=true"
-  });
-  return request(fetchUrl);
 }
